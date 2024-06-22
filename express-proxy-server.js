db363dd3649069e0433b82bef36c999f39fd6a19
ulfapivis <ulf@apivis.com>
2024-06-22 23:13:49 +0200

    Update express-proxy-server.js

--- express-proxy-server.js
+++ express-proxy-server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
-app.set('trust proxy', 1); // This will trust the reverse proxy's IP address
+// app.set('trust proxy', 1); // This will trust the reverse proxy's IP address
+app.set('trust proxy', true);
 app.use(helmet());
 app.use(cors());
 
@@ -25,34 +26,42 @@ const csp = {
 
 app.use(helmet.contentSecurityPolicy(csp));
 
-const allowedIps = process.env.ALLOWED_IPS.split(',');
+const allowedIps = process.env.ALLOWED_IPS
+// .split(',');
 
 // Middleware to check if the request is from an allowed IP
-app.use((req, res, next) => {
-    let ips = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',') : [req.socket.remoteAddress];
-    ips = ips.map(ip => ip.trim()); // Trim whitespace from each IP
-    console.log(`Incoming request from IPs: ${ips.join(', ')}`);
+// app.use((req, res, next) => {
+//     let ips = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',') : [req.socket.remoteAddress];
+//     ips = ips.map(ip => ip.trim()); // Trim whitespace from each IP
+//     console.log(`Incoming request from IPs: ${ips.join(', ')}`);
     
-    const allowed = ips.some(ip => allowedIps.includes(ip));
+//     const allowed = ips.some(ip => allowedIps.includes(ip));
     
-    if (allowed) {
-      console.log(`At least one IP in ${ips.join(', ')} is allowed`);
-      next();
-    } else {
-      console.log(`None of the IPs in ${ips.join(', ')} are allowed`);
-      res.status(403).send('Forbidden');
-    }
-});
+//     if (allowed) {
+//       console.log(`At least one IP in ${ips.join(', ')} is allowed`);
+//       next();
+//     } else {
+//       console.log(`None of the IPs in ${ips.join(', ')} are allowed`);
+//       res.status(403).send('Forbidden');
+//     }
+// });
 
-app.use('/stripe', createProxyMiddleware({ 
-  target: 'http://stripe-server:3000', 
-  changeOrigin: true,
-  ws: true,  // Enable WebSocket proxying
-  onError: (err, req, res) => {
-    console.error('Proxy error:', err);
-    res.status(500).send('stripe server proxy error.');
-  },
-}));
+app.use((req, res, next) => {
+  // Extract the IP address
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log(`Incoming request from IP: ${ip}`);
+  
+  // Check if the request is from an allowed IP
+  if (allowedIps.includes(ip)) {
+    console.log(`IP ${ip} is allowed`);
+    req.allowedIp = true;  // Add a new property to the request
+    next();
+  } else {
+    console.log(`IP ${ip} is not allowed`);
+    req.allowedIp = false;  // Add a new property to the request
+    res.status(403).send('Forbidden');
+  }
+});
 
 app.use('/node-red', createProxyMiddleware({ 
   target: 'http://node-red:1880',
