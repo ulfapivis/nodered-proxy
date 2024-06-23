9a89828a4b237208ce2c96acf2ea3456d088c515
ulfapivis <ulf@apivis.com>
2024-06-23 22:01:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -304,10 +304,10 @@ module.exports = {
          * See https://github.com/troygoode/node-cors#configuration-options for
          * details on its contents. The following is a basic permissive set of options:
          */
-        httpNodeCors: {
-           origin: "*",
-           methods: "GET,PUT,POST,DELETE"
-        },
+        // httpNodeCors: {
+        //    origin: "*",
+        //    methods: "GET,PUT,POST,DELETE"
+        // },
         // httpNodeCors: {
         //     origin: "*",
         //     methods: "GET,PUT,POST,DELETE",
