a37ef483f15d1098820055cfe0213db58f65d179
ulfapivis <ulf@apivis.com>
2024-06-24 00:08:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+
 # Load flow data from the environmental variable
 FLOW_DATA="$NODE_RED_FLOW"
+echo "Flow data received: $FLOW_DATA"
 
 # Copy the flow data to the /data directory
 echo "$FLOW_DATA" > /data/flows.json
 
+# Verify if the file was created
+ls -l /data/flows.json
+
+# Install custom nodes if specified
 if [ -n "${CUSTOM_NODES}" ]; then
-    # If not empty, install the custom nodes
     npm install $(echo ${CUSTOM_NODES})
 else
     echo "No custom nodes specified"
 fi
 
 # Start Node-RED
-node-red -s /data/settings.js
+node-red -s /settings.js
+
 
 
