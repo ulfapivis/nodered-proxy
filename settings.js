576cf10307816540f42041bab8f784c8b15adc51
ulfapivis <ulf@apivis.com>
2024-06-23 20:23:33 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,19 +28,39 @@ console.log('ALLOWED_IPS :', process.env.ALLOWED_IPS);
 const bcrypt = require('bcrypt');
 
 // uiHost: process.env.ALLOWED_IPS.split(','),
-const allowedIPs = process.env.ALLOWED_IPS.split(',');
+// const allowedIPs = process.env.ALLOWED_IPS.split(',');
+// const { BlockList } = require("net");
+// const WL = new BlockList();
+// allowedIPs.forEach((v, i, a) => {
+//   if (v.includes("/")) {
+//     const Parts = v.split("/");
+//     WL.addSubnet(Parts[0].trim(), parseInt(Parts[1].trim()));
+//   } else if (v.includes("-")) {
+//     const Parts = v.split("-");
+//     WL.addRange(Parts[0].trim(), Parts[1].trim());
+//   } else {
+//     WL.addAddress(v.trim());
+//   }
+// });
+
+
+
+
+
+const allowedIPs = process.env.ALLOWED_IPS.split(',').map(ip => ip.trim());
 const { BlockList } = require("net");
 const WL = new BlockList();
+
 allowedIPs.forEach((v, i, a) => {
-  if (v.includes("/")) {
-    const Parts = v.split("/");
-    WL.addSubnet(Parts[0].trim(), parseInt(Parts[1].trim()));
-  } else if (v.includes("-")) {
-    const Parts = v.split("-");
-    WL.addRange(Parts[0].trim(), Parts[1].trim());
-  } else {
-    WL.addAddress(v.trim());
-  }
+    if (v.includes("/")) {
+        const Parts = v.split("/");
+        WL.addSubnet(Parts[0].trim(), parseInt(Parts[1].trim()));
+    } else if (v.includes("-")) {
+        const Parts = v.split("-");
+        WL.addRange(Parts[0].trim(), Parts[1].trim());
+    } else {
+        WL.addAddress(v.trim());
+    }
 });
 
 module.exports = {
